79727da7e110221b951c0d15a86e7234917b2d6d
ponz <83156814+1217pond@users.noreply.github.com>
2024-06-24 14:30:58 +0900

    new

--- src/guest/script.js
+++ src/guest/script.js
@@ -1,15 +1,15 @@
 class GUEST_CONNECTION {
-	constructor(before_UUID = !1, restart = !1) {
+	constructor(before_UUID = false, restart = false) {
 		this.restart = restart;
 		this.before_UUID = before_UUID;
 		this.NEXT_STAGE_CONDITION = "wait"
 		this.NOW_STAGE_ID = 0
-		this.CORRECTED_ANS = !1
+		this.CORRECTED_ANS = false
 		this.STAGES = ["entrance", "laser", "mystery_1", "box", "mystery_2", "trump", "mystery_last", "CLEAR", ]
 		this.STAGES_DIV_QUERY = ["#stage_ent", "#stage_A", "#stage_B", "#stage_C", "#stage_D", "#stage_E", "#stage_last", "#stage_clear"]
 		this.MYSTERY_STAGES = [2, 4, 6]
 		this.MYSTERY_ANSWERS = ["", "", "answer", "", "answer", "", "answer", ""]
-		this.start()
+		this.start();
 	}
 	async start() {
 		if(this.before_UUID && !this.restart) {
@@ -19,6 +19,8 @@ class GUEST_CONNECTION {
 		}
 		$("#uuid").text(this.UUID);
 		if(this.restart) {
+			$("#connecting").show();
+			$("#connecting #state_indicator").text("リスタート中");
 			try {
 				let response = await fetch(`https://script.google.com/macros/s/AKfycbwMbUDB-Gdqrm9grC9nx9evBZG59NTOiT58YueT1LVB_BEOwaBun6DvarLmwlKPtoec/exec?type=kick&trg_uuid=${this.before_UUID}`, {
 					method: "GET",
@@ -26,8 +28,9 @@ class GUEST_CONNECTION {
 				})
 			} catch(e) {
 				$("#connecting_crash").show();
-				throw e
+				throw e;
 			}
+			$("#connecting").hide();
 		}
 		if(this.before_UUID && !this.restart) {
 			$("#connecting").show();
@@ -47,9 +50,9 @@ class GUEST_CONNECTION {
 				}
 			}
 			console.log("move_to", trg_stage_id);
-			$("#reload_state").attr("disabled", !1);
+			$("#reload_state").attr("disabled", false);
 			if(trg_stage_id != 0) {
-				let result = await this.move_to(trg_stage_id, !1);
+				let result = await this.move_to(trg_stage_id, false);
 				if(result) {
 					$("#connecting_crash").show();
 					console.log(result);
@@ -57,8 +60,8 @@ class GUEST_CONNECTION {
 				}
 			} else {
 				await alert_org("サーバー上に記録が見つからなかったため、最初から始めます。");
-				this.before_UUID = !1;
-				this.restart = !1;
+				this.before_UUID = false;
+				this.restart = false;
 				start();
 				return
 			}
@@ -73,7 +76,7 @@ class GUEST_CONNECTION {
 				$("#connecting_crash").show();
 				throw e
 			}
-			$("#reload_state").attr("disabled", !1);
+			$("#reload_state").attr("disabled", false);
 			if(stage_state.laser.is_used) {
 				$("#indicator").removeClass("stage_condition_wait");
 				$("#indicator").addClass("stage_condition_full");
@@ -82,7 +85,7 @@ class GUEST_CONNECTION {
 				$("#indicator").removeClass("stage_condition_wait");
 				$("#indicator").addClass("stage_condition_empty");
 				$("#indicator").text("空き");
-				$("#go_next_stage").attr("disabled", !1)
+				$("#go_next_stage").attr("disabled", false)
 			}
 		}
 		$("#go_next_stage").on("click", async() => {
@@ -127,22 +130,22 @@ class GUEST_CONNECTION {
 		});
 		$("#answer_field").on("input", () => {
 			if($("#answer_field").val()) {
-				$("#answer").attr("disabled", !1)
+				$("#answer").attr("disabled", false)
 			} else {
-				$("#answer").attr("disabled", !0)
+				$("#answer").attr("disabled", true)
 			}
 		});
 		$("#answer").on("click", () => {
 			if($("#answer_field").val() == this.MYSTERY_ANSWERS[this.NOW_STAGE_ID]) {
 				alert_org("正解！");
-				this.CORRECTED_ANS = !0;
+				this.CORRECTED_ANS = true;
 				if(this.NOW_STAGE_ID != 6) {
-					$("#go_next_stage").attr("disabled", !1)
+					$("#go_next_stage").attr("disabled", false)
 				} else {
-					$("#escape").attr("disabled", !1)
+					$("#escape").attr("disabled", false)
 				}
-				$("#answer").attr("disabled", !0);
-				$("#answer_field").attr("disabled", !0)
+				$("#answer").attr("disabled", true);
+				$("#answer_field").attr("disabled", true)
 			} else {
 				alert_org("不正解...")
 			}
@@ -155,14 +158,14 @@ class GUEST_CONNECTION {
 			let trg_uuid = `${$("#uuid-enter-1").val()}-${$("#uuid-enter-2").val()}-${$("#uuid-enter-3").val()}-${$("#uuid-enter-4").val()}-${$("#uuid-enter-5").val()}`;
 			if(trg_uuid.length == 36) {
 				try {
-					$("#reloader #allow").attr("disabled", !0);
-					$("#reloader #reject").attr("disabled", !0);
+					$("#reloader #allow").attr("disabled", true);
+					$("#reloader #reject").attr("disabled", true);
 					let stage_state = await this.get_stage_state();
 					console.log();
-					let is_exist = !1;
+					let is_exist = false;
 					for(let room_name in stage_state) {
 						if(stage_state[room_name].uuid == trg_uuid) {
-							is_exist = !0;
+							is_exist = true;
 							break
 						}
 					}
@@ -171,16 +174,16 @@ class GUEST_CONNECTION {
 						Cookies.set('UUID', trg_uuid, {
 							expires: 0.5
 						});
-						window.location.reload(!1)
+						window.location.reload(false)
 					} else {
 						alert("そのUUIDは存在しません。")
 					}
-					$("#reloader #allow").attr("disabled", !1);
-					$("#reloader #reject").attr("disabled", !1)
+					$("#reloader #allow").attr("disabled", false);
+					$("#reloader #reject").attr("disabled", false)
 				} catch(e) {
 					alert(e.message);
-					$("#reloader #allow").attr("disabled", !1);
-					$("#reloader #reject").attr("disabled", !1)
+					$("#reloader #allow").attr("disabled", false);
+					$("#reloader #reject").attr("disabled", false)
 				}
 			} else {
 				alert("文字数が異なります。")
@@ -195,19 +198,19 @@ class GUEST_CONNECTION {
 		$("#indicator").removeClass("stage_condition_empty");
 		$("#indicator").removeClass("stage_condition_full");
 		$("#indicator").addClass("stage_condition_wait");
-		$("#go_next_stage").attr("disabled", !0);
-		$("#reload_state").attr("disabled", !0);
+		$("#go_next_stage").attr("disabled", true);
+		$("#reload_state").attr("disabled", true);
 		let stage_state;
 		try {
 			stage_state = await this.get_stage_state()
 		} catch(e) {
 			$("#indicator").text("取得失敗");
 			alert_org("通信エラーが発生しました。通信環境の良い状態で再取得してください。");
-			$("#reload_state").attr("disabled", !1);
+			$("#reload_state").attr("disabled", false);
 			return
 		}
 		this.indicate_state(stage_state, this.NOW_STAGE_ID);
-		$("#reload_state").attr("disabled", !1)
+		$("#reload_state").attr("disabled", false)
 	}
 	async get_stage_state() {
 		let response = await fetch(`https://script.google.com/macros/s/AKfycbwMbUDB-Gdqrm9grC9nx9evBZG59NTOiT58YueT1LVB_BEOwaBun6DvarLmwlKPtoec/exec?type=state`, {
@@ -216,7 +219,7 @@ class GUEST_CONNECTION {
 		});
 		return await response.json()
 	}
-	async move_to(trg_stage_id, ignore_error = !0) {
+	async move_to(trg_stage_id, ignore_error = true) {
 		$(this.STAGES_DIV_QUERY[this.NOW_STAGE_ID]).hide();
 		$(this.STAGES_DIV_QUERY[trg_stage_id]).show();
 		this.NOW_STAGE_ID = trg_stage_id;
@@ -224,13 +227,13 @@ class GUEST_CONNECTION {
 		$("#indicator").removeClass("stage_condition_empty");
 		$("#indicator").removeClass("stage_condition_full");
 		$("#indicator").addClass("stage_condition_wait");
-		$("#go_next_stage").attr("disabled", !0);
-		$("#reload_state").attr("disabled", !0);
+		$("#go_next_stage").attr("disabled", true);
+		$("#reload_state").attr("disabled", true);
 		if(this.MYSTERY_STAGES.includes(this.NOW_STAGE_ID)) {
-			this.CORRECTED_ANS = !1;
+			this.CORRECTED_ANS = false;
 			$("#answer_field").val("");
-			$("#answer").attr("disabled", !0);
-			$("#answer_field").attr("disabled", !1);
+			$("#answer").attr("disabled", true);
+			$("#answer_field").attr("disabled", false);
 			$("#answer_area").show()
 		} else {
 			$("#answer_area").hide()
@@ -278,10 +281,10 @@ class GUEST_CONNECTION {
 			} catch(e) {
 				$("#indicator").text("取得失敗");
 				alert_org("通信エラーが発生しました。通信環境の良い状態で再取得してください。");
-				$("#reload_state").attr("disabled", !1);
+				$("#reload_state").attr("disabled", false);
 				return
 			}
-			$("#reload_state").attr("disabled", !1);
+			$("#reload_state").attr("disabled", false);
 			this.indicate_state(stage_state, this.NOW_STAGE_ID)
 		}
 	}
@@ -294,15 +297,15 @@ class GUEST_CONNECTION {
 				$("#indicator").removeClass("stage_condition_wait");
 				$("#indicator").addClass("stage_condition_full");
 				$("#indicator").text("使用中");
-				$("#go_next_stage").attr("disabled", !0)
+				$("#go_next_stage").attr("disabled", true)
 			} else {
 				$("#indicator").removeClass("stage_condition_wait");
 				$("#indicator").addClass("stage_condition_empty");
 				$("#indicator").text("空き");
 				if(this.MYSTERY_STAGES.includes(now_stage_id) && !this.CORRECTED_ANS) {
-					$("#go_next_stage").attr("disabled", !0)
+					$("#go_next_stage").attr("disabled", true)
 				} else {
-					$("#go_next_stage").attr("disabled", !1)
+					$("#go_next_stage").attr("disabled", false)
 				}
 			}
 		}
@@ -313,10 +316,10 @@ window.onload = () => {
 		if(Cookies.get("UUID")) {
 			const conf = async() => {
 				if(await confirm_org("前回の接続情報が残っています。継承しますか？")) {
-					window.CONNECTING_INSTANCE = new GUEST_CONNECTION(Cookies.get("UUID"))
+					window.CONNECTING_INSTANCE = new GUEST_CONNECTION(Cookies.get("UUID"));
 				} else {
 					if(await confirm_org("本当に最初から始めますか？")) {
-						window.CONNECTING_INSTANCE = new GUEST_CONNECTION(Cookies.get("UUID"), !0);
+						window.CONNECTING_INSTANCE = new GUEST_CONNECTION(Cookies.get("UUID"), true);
 						Cookies.remove('UUID')
 					} else {
 						conf()
@@ -341,14 +344,14 @@ function confirm_org(message) {
 			$("#confirm #reject").off("click");
 			$("#confirm span").text("");
 			$("#confirm").hide();
-			resolve(!0)
+			resolve(true)
 		});
 		$("#confirm #reject").on("click", () => {
 			$("#confirm #allow").off("click");
 			$("#confirm #reject").off("click");
 			$("#confirm span").text("");
 			$("#confirm").hide();
-			resolve(!1)
+			resolve(false)
 		})
 	})
 }
@@ -360,7 +363,7 @@ function alert_org(message) {
 		$("#alert #allow").one("click", () => {
 			$("#alert span").text("");
 			$("#alert").hide();
-			resolve(!0)
+			resolve(true)
 		})
 	})
 }
